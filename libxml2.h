d4e5df66c51786a492804eecd240fc5d6ee2f8e3
Yusuke Ito <novi.mad@gmail.com>
2016-07-11 17:43:48 +0900

    fix for Linux

--- libxml2.h
+++ libxml2.h
@@ -1,4 +1,8 @@
 #include <libxml2/libxml/globals.h>
 #include <libxml2/libxml/HTMLtree.h>
 #include <libxml2/libxml/xpath.h>
+#ifdef __linux__
+#include <libxml2/libxml/xpathInternals.h>
+#else
 #include <libxml2/libxml/xpathinternals.h>
+#endif
