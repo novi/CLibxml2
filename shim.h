72ee405ae92fec2f335c2d650c64ac99c705d352
Yusuke Ito <novi.mad@gmail.com>
2019-02-26 20:07:22 +0900

    fix paths

--- shim.h
+++ shim.h
@@ -1,4 +1,4 @@
-#include <libxml/globals.h>
-#include <libxml/HTMLtree.h>
-#include <libxml/xpath.h>
-#include <libxml/xpathInternals.h>
+#include <libxml2/libxml/globals.h>
+#include <libxml2/libxml/HTMLtree.h>
+#include <libxml2/libxml/xpath.h>
+#include <libxml2/libxml/xpathInternals.h>
